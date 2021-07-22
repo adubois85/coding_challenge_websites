b1b0df267fff45b744eda7637d7a3616efe7ad66
Alex DuBois <adubois@alumni.uci.edu>
2021-07-22 13:25:58 -0600

    initial python solution to LC#13

--- LeetCode/solution-python.py
+++ LeetCode/solution-python.py
@@ -1,3 +1,14 @@
+from collections import deque
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        romans = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+        sum = 0
+        deq = deque(s)
+        while deq:
+            x = romans[deq.popleft()]
+            y = 0
+            if x in {"I", "X", "C"} and 0 < (romans[deq[0]] - x) < (x * 10):
+                x = x * -1
+                y = romans[deq.popleft()]
+            sum = sum + x + y
+        return sum
