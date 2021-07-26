0952135deb11c988cbe9690564eb58f0e7ab3ac1
Alex DuBois <adubois@alumni.uci.edu>
2021-07-25 18:34:34 -0600

    working(but slow) python solution to LC#53

--- LeetCode/1-1000/1-100/50-75/53. Maximum Subarray/solution-python.py
+++ LeetCode/1-1000/1-100/50-75/53. Maximum Subarray/solution-python.py
@@ -4,10 +4,21 @@ from typing import List
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
         # naive
-        if len(nums) == 1:
+        # if len(nums) == 1:
+        #     return nums[0]
+        # sum = 0
+        # for i, j in zip(nums, nums[1:]):
+        #     if i + j > -1:
+        #         sum += j
+        # return sum
+        # super naive
+        if len(nums) = 1:
             return nums[0]
         sum = 0
-        for i, j in zip(nums, nums[1:]):
-            if i + j > -1:
-                sum += j
+        for i in range(len(nums)):
+            temp = 0
+            for j in range(i, len(nums)):
+                temp += nums[j]
+                if temp > sum:
+                    sum = temp
         return sum
