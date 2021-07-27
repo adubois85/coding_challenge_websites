fc1056dcb505f471a4ed8fd557a5322f1caf72f4
Alex DuBois <adubois@alumni.uci.edu>
2021-07-27 14:55:11 -0600

    python solution for LC#88 works now

--- LeetCode/1-1000/1-100/76-100/88. Merge Sorted Array/solution-python.py
+++ LeetCode/1-1000/1-100/76-100/88. Merge Sorted Array/solution-python.py
@@ -7,10 +7,19 @@ class Solution:
         Do not return anything, modify nums1 in-place instead.
         """
         # it feels like cheating using Python built-in stuff, but...
-        if not nums2 or n == 0:
-            nums1 = nums1[0:m]
-        elif not m == 0:
-            nums1 = nums2[0:n]
-        else:
-            nums1 = nums1[0:m] + nums2[0:n]
-            nums1.sort()
+        # if not nums2 or n == 0:
+        #     nums1 = nums1[0:m]
+        # elif not m == 0:
+        #     nums1 = nums2[0:n]
+        # else:
+        #     temp = nums1[0:m] + nums2[0:n]
+        #     temp.sort()
+        #     nums1 = [v for v in temp]
+
+        # stupid Python copying of variables instead of altering things
+        # in place as one would expect
+        for i in range(len(nums1) - m):
+            nums1.pop()
+        for i in nums2[0:n]:
+            nums1.append(i)
+        nums1.sort()
