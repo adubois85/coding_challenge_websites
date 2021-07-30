4adad7d049fa74cfa39c14f50fe6aa0af507f668
Alex DuBois <adubois@alumni.uci.edu>
2021-07-29 18:02:15 -0600

    some work on python solution to LC#155

--- LeetCode/1-1000/101-200/151-175/155. Min Stack/solution-python.py
+++ LeetCode/1-1000/101-200/151-175/155. Min Stack/solution-python.py
@@ -4,9 +4,23 @@ class MinStack:
         """
         initialize your data structure here.
         """
+        # Current top of stack
+        self.top_val = None
+        # Next item down the stack
+        # Should end up being a tuple of tuples
+        self.previous_val = None
+        # Current smallest value on the stack
+        self.min_val = None
+        # Need to consider the number of times an item has been added to the stack
+        self.val_counts = {}
 
     def push(self, val: int) -> None:
-        pass
+        self.val_counts.setdefault(val, 0)
+        self.val_counts[val] += 1
+        if val < self.min_val:
+            self.min_val = val
+        self.top_val = val
+        self.previous_val = (self.top_val, self.previous_val)
 
     def pop(self) -> None:
         pass
