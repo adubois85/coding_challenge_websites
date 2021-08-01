6179fa2e70cf897af8371b57b356d2048289f3df
Alex DuBois <adubois@alumni.uci.edu>
2021-07-31 18:00:19 -0600

    the most naive Python solution to LC#169

--- LeetCode/1-1000/101-200/151-175/169. Majority Element/solution-python.py
+++ LeetCode/1-1000/101-200/151-175/169. Majority Element/solution-python.py
@@ -3,4 +3,9 @@ from typing import List
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        pass
+        # naive to the extreme
+        num_set = {}
+        for i in nums:
+            num_set.setdefault(i, 0)
+            num_set[i] += 1
+        return list(num_set.keys())[list(num_set.values()).index(max(num_set.values()))]
