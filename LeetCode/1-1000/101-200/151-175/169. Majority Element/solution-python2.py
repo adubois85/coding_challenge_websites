80f1f8fd5b6fc43f6bdaea9e6c63f5fb89ff3069
Alex DuBois <adubois@alumni.uci.edu>
2021-08-01 17:20:43 -0600

    sort of Boyer-Moore majority alorithm, LC#169

--- LeetCode/1-1000/101-200/151-175/169. Majority Element/solution-python2.py
+++ LeetCode/1-1000/101-200/151-175/169. Majority Element/solution-python2.py
@@ -1,7 +1,23 @@
 from typing import List
+from collections import deque
 
 
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         # Let's try to do this in a more sensible way...
-        pass
+
+        # if there are only 1 or 2 elements in the list
+        # the first item is necessarily the majority
+        if len(nums) < 3:
+            return nums[0]
+        deq = deque(nums)
+        temp = []
+        current_val = deq.popleft()
+        while deq:
+            if current_val != deq[0]:
+                deq.popleft()
+                current_val = temp.pop() if temp else deq.popleft()
+            else:
+                temp.append(current_val)
+                current_val = deq.popleft()
+        return current_val
