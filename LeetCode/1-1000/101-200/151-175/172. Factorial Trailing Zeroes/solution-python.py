41b87d9ce328e2d6caa3619020acd1cc86370743
Alex DuBois <adubois@alumni.uci.edu>
2021-08-01 20:14:50 -0600

    slow, brute force Python solution for LC#172

--- LeetCode/1-1000/101-200/151-175/172. Factorial Trailing Zeroes/solution-python.py
+++ LeetCode/1-1000/101-200/151-175/172. Factorial Trailing Zeroes/solution-python.py
@@ -1,3 +1,14 @@
+from math import factorial
+
+
 class Solution:
     def trailingZeroes(self, n: int) -> int:
-        pass
+        # LOL brute-force
+        # but yes, there's a better way
+        if n < 5:
+            return 0
+        f = factorial(n)
+        counter = 0
+        while f % (10 ** (counter + 1)) == 0:
+            counter += 1
+        return counter
