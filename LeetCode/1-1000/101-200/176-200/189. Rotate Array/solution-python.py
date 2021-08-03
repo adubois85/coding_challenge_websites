2906ccd6a4dae79aa2494ba8aa609b1a840ecbbd
Alex DuBois <adubois@alumni.uci.edu>
2021-08-02 18:47:08 -0600

    obvious and slow Python solution to LC#189

--- LeetCode/1-1000/101-200/176-200/189. Rotate Array/solution-python.py
+++ LeetCode/1-1000/101-200/176-200/189. Rotate Array/solution-python.py
@@ -6,4 +6,20 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        pass
+        # the most obvious, but not best, way is to create a
+        # second list startting from the rotated value,
+        # then copy that back onto the original list
+        temp = []
+        # Rotating the list by the number of items in it is the
+        # same as doing nothing.  Rotating by more that that is
+        # then the same as rotating it by the modulus of those
+        if k >= len(nums):
+            k = k % len(nums)
+        rot_end = len(nums) - k
+        while k > 0:
+            temp.append(nums[-k])
+            k -= 1
+        for i in range(rot_end):
+            temp.append(nums[i])
+        for i in range(len(nums)):
+            nums[i] = temp[i]
