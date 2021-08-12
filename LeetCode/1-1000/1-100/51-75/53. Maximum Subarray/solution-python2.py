afc3464fe6f55a018baa68dfe24c207061d9bda1
Alex DuBois <adubois@alumni.uci.edu>
2021-08-12 16:17:47 -0600

    DP Python solution for LC#53

--- LeetCode/1-1000/1-100/51-75/53. Maximum Subarray/solution-python2.py
+++ LeetCode/1-1000/1-100/51-75/53. Maximum Subarray/solution-python2.py
@@ -3,5 +3,14 @@ from typing import List
 
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        
-        pass
+        # This is a dynamic programming problem.  The tricky part, as with all
+        # dynamic programming problems, is how to setup your subproblems such
+        # that you don't create cyclic dependencies or exponential time growth
+        # where you only need linear time.
+        # About the most concise I can make this, using Python's version of
+        # the ternary operator, aka conditional expressions
+        current_sum = max_sum = nums[0]
+        for i in range(1, len(nums)):
+            current_sum = max(current_sum + nums[i], nums[i])
+            max_sum = current_sum if current_sum > max_sum else max_sum
+        return max_sum
