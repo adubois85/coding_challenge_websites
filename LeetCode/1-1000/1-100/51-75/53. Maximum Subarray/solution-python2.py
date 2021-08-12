f405cc1c7da7114b2c658df55679904e7cd1e95c
Alex DuBois <adubois@alumni.uci.edu>
2021-08-12 16:48:13 -0600

    faster but less concise change to 2nd Python LC#53

--- LeetCode/1-1000/1-100/51-75/53. Maximum Subarray/solution-python2.py
+++ LeetCode/1-1000/1-100/51-75/53. Maximum Subarray/solution-python2.py
@@ -12,5 +12,11 @@ class Solution:
         current_sum = max_sum = nums[0]
         for i in range(1, len(nums)):
             current_sum = max(current_sum + nums[i], nums[i])
-            max_sum = current_sum if current_sum > max_sum else max_sum
+            # it seems to be slightly faster if you use a regular if block
+            # instead of the ternary operator, probably because it has to
+            # set the variable, even if it's unchanged, whereas the if
+            # statement only sets if it needs to.
+            if current_sum > max_sum:
+                max_sum =  current_sum
+            # max_sum = current_sum if current_sum > max_sum else max_sum
         return max_sum
