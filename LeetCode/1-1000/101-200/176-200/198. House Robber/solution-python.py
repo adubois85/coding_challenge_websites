536f172748500d0681ff3a240fb373273bec61e9
Alex DuBois <adubois@alumni.uci.edu>
2021-08-12 19:10:08 -0600

    Python solution for LC#198

--- LeetCode/1-1000/101-200/176-200/198. House Robber/solution-python.py
+++ LeetCode/1-1000/101-200/176-200/198. House Robber/solution-python.py
@@ -3,4 +3,17 @@ from typing import List
 
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        pass
+        # This is another dynamic programming problem.  This is very similar to
+        # LeetCode problem #53, Maximum Subarray.  There, you were basically
+        # asking what was the largest sum possible for the subarray ending on
+        # that index of the main array.  Here, you are asking more or less the
+        # same thing, with the caveat that you have to skip the adjacent index.
+        if len(nums) == 1:
+            return nums[0]
+        one_back = nums[0]
+        two_back = current = 0
+        for i in range(1, len(nums)):
+            current = max(nums[i] + two_back, one_back)
+            two_back = one_back
+            one_back = current
+        return current
