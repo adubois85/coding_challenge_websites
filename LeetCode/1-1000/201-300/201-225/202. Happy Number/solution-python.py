254807725cd4d60b6968b2ad7562cf36bd703ce0
Alex DuBois <adubois@alumni.uci.edu>
2021-08-13 17:08:50 -0600

    completed Python solution for LC#202

--- LeetCode/1-1000/201-300/201-225/202. Happy Number/solution-python.py
+++ LeetCode/1-1000/201-300/201-225/202. Happy Number/solution-python.py
@@ -4,7 +4,7 @@ class Solution:
     # conversion method below.  On the other hand, this is probably more
     # portable as not every language lets you swap between strings and ints
     # so easily
-    def int_split(num: int) -> list:
+    def int_split(self, num: int) -> list:
         units = []
         digit = 10
         while num != 0:
@@ -15,9 +15,18 @@ class Solution:
             digit *= 10
         return units
 
-    def isHappy(self, n: int) -> bool:
-        temp = list(str(n))
+    def split_sum(self, num: int) -> int:
+        temp = list(str(num))
         for i in range(len(temp)):
             x = int(temp[i])
             temp[i] = x * x
-        return temp
+        return sum(temp)
+
+    def isHappy(self, n: int) -> bool:
+        values = []
+        while n not in values:
+            values.append(n)
+            n = self.split_sum(n)
+        if n == 1:
+            return True
+        return False
