01b75e62e28a40ad2f5d881368ddc4db5c17dc66
Alex DuBois <adubois@alumni.uci.edu>
2021-08-14 17:53:19 -0600

    slow but functional Python solution for LC#204

--- LeetCode/1-1000/201-300/201-225/204. Count Primes/solution-python.py
+++ LeetCode/1-1000/201-300/201-225/204. Count Primes/solution-python.py
@@ -1,3 +1,18 @@
 class Solution:
     def countPrimes(self, n: int) -> int:
-        pass
+        # doing things the hard way
+        nums = set()
+        count = 0
+        for i in range(2, n):
+            # print(i)
+            if i in nums:
+                pass
+            else:
+                count += 1
+                nums.add(i)
+                j = i * i
+                nums.add(j)
+                while j < n:
+                    j += i
+                    nums.add(j)
+        return count
