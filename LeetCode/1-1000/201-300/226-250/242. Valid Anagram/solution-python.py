3916a425f925e0310bdf638612ef7d9f81ed706c
Alex DuBois <adubois@alumni.uci.edu>
2021-08-18 16:17:07 -0600

    janky list sorting Python solution for LC#242

--- LeetCode/1-1000/201-300/226-250/242. Valid Anagram/solution-python.py
+++ LeetCode/1-1000/201-300/226-250/242. Valid Anagram/solution-python.py
@@ -1,3 +1,11 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        pass
+        # if the strings aren't the same length, they can't be anagrams
+        if len(s) != len(t):
+            return False
+        # creating lists from each string and sorting them should result in the
+        # same list if they're anagrams; should also be robust and work for any
+        # Unicode string, not just lowercase Latin characters.
+        if sorted(list(s)) != sorted(list(t)):
+            return False
+        return True
