a3acb19570e7fab7e0f4e79a0197ca8405e95208
Alex DuBois <adubois@alumni.uci.edu>
2021-08-19 00:07:46 -0600

    initial work on helper functions, LC#371

--- LeetCode/1-1000/301-400/351-375/371. Sum of Two Integers/solution-python.py
+++ LeetCode/1-1000/301-400/351-375/371. Sum of Two Integers/solution-python.py
@@ -1,3 +1,16 @@
+from typing import List
+
+
 class Solution:
+    # helper function to return the two's complement binary representation of
+    # a number as a list of single character strings
+    def twos_complement(n: List) -> List:
+        n = list(f'{n:011b}')
+        for i in range(len(n)):
+            n[i] = ('0' if n[i] == '1' else '1')
+        # bin_add(n, 1)
+        return n
+
+
     def getSum(self, a: int, b: int) -> int:
         pass
