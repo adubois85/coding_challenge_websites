00ef090c7c5fa72a0e9d04b29cc472ffeddaa72b
Alex DuBois <adubois@alumni.uci.edu>
2021-08-20 16:04:22 -0600

    Python solution for LC#557 using builtin str manip

--- LeetCode/1-1000/501-600/551-575/557. Reverse Words in a String III/solution-python.py
+++ LeetCode/1-1000/501-600/551-575/557. Reverse Words in a String III/solution-python.py
@@ -1,3 +1,9 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        pass
+        # Using Python's built-in string manipulation methods
+        # Split the string into words at the spaces, reverse and join the
+        # individual words, then rejoin the words with a space
+        s = s.split(' ')
+        for i, word in enumerate(s):
+            s[i] = ''.join(list(reversed(word)))
+        return ' '.join(s)
