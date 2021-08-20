66b6ce259ed72b873abef6e9e945ad4a14f6e5c1
Alex DuBois <adubois@alumni.uci.edu>
2021-08-20 14:39:22 -0600

    added comment for first solution to LC#448

--- LeetCode/1-1000/401-500/426-450/448. Find All Numbers Disappeared in an Array/solution-python.py
+++ LeetCode/1-1000/401-500/426-450/448. Find All Numbers Disappeared in an Array/solution-python.py
@@ -4,6 +4,12 @@ from collections import Counter
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
+        # Should be pretty self evident: create a counter for all the items,
+        # then check if every number in the inclusive range(1, n) has a key in
+        # the counter.  Probably because of how the keys are hashed, trying to
+        # do this on the input list (e.g. if i not in nums) is significantly
+        # slower than using the counter, even accounting for the time it takes
+        # to build said counter.
         x = Counter(nums)
         temp = []
         for i in range(1, len(nums) + 1):
