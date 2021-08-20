5587b0056b556c223fb3dfd87706748e11401268
Alex DuBois <adubois@alumni.uci.edu>
2021-08-20 14:19:38 -0600

    Python solution using a Counter for LC#448

--- LeetCode/1-1000/401-500/426-450/448. Find All Numbers Disappeared in an Array/solution-python.py
+++ LeetCode/1-1000/401-500/426-450/448. Find All Numbers Disappeared in an Array/solution-python.py
@@ -1,6 +1,12 @@
 from typing import List
+from collections import Counter
 
 
 class Solution:
     def findDisappearedNumbers(self, nums: List[int]) -> List[int]:
-        pass
+        x = Counter(nums)
+        temp = []
+        for i in range(1, len(nums) + 1):
+            if i not in x:
+                temp.append(i)
+        return temp
