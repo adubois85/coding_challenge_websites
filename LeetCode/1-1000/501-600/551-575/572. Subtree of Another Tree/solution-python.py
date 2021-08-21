3ec7af00ece2f030a4408897672e33e1a20ea326
Alex DuBois <adubois@alumni.uci.edu>
2021-08-21 14:59:07 -0600

    Python solution, with assist, for LC#572

--- LeetCode/1-1000/501-600/551-575/572. Subtree of Another Tree/solution-python.py
+++ LeetCode/1-1000/501-600/551-575/572. Subtree of Another Tree/solution-python.py
@@ -10,5 +10,19 @@ class TreeNode:
 
 
 class Solution:
+    # With a heavy assist from this video:
+    # https://www.youtube.com/watch?v=HdMs2Fl_I-Q
+    # Still feels like there should be a way to do this without a helper function
     def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
-        pass
+        if not root:
+            return False
+        if self.is_same(root, subRoot):
+            return True
+        return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+
+    def is_same(self, tree, subTree):
+        if not tree or not subTree:
+            return tree is None and subTree is None
+        if tree.val == subTree.val:
+            return self.is_same(tree.left, subTree.left) and self.is_same(tree.right, subTree.right)
+        return False
