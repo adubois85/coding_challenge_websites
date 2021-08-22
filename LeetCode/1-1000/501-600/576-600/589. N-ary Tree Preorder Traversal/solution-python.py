cbaefb76d043da4c02fbf2180705a20ee1d241f1
Alex DuBois <adubois@alumni.uci.edu>
2021-08-21 18:33:44 -0600

    not quite functional solution for LC#589

--- LeetCode/1-1000/501-600/576-600/589. N-ary Tree Preorder Traversal/solution-python.py
+++ LeetCode/1-1000/501-600/576-600/589. N-ary Tree Preorder Traversal/solution-python.py
@@ -9,5 +9,13 @@ class Node:
 
 
 class Solution:
+    # Not quite functional yet, returns list of lists of lists with no simple
+    # way to flatten them that's obvious to me.  Putting this on the shelf for
+    # bit to work on the next problem
     def preorder(self, root: 'Node') -> List[int]:
-        pass
+        order = [root.val]
+        if root.children:
+            for i, leaf in enumerate(root.children):
+                # print(i, leaf)
+                order.append(self.preorder(leaf))
+        return order
