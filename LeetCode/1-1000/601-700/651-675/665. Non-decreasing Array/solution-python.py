ff2d4a1aefe79d91700ea053c7e4e3cffba429da
Alex DuBois <adubois@alumni.uci.edu>
2021-08-23 17:59:34 -0600

    ugly, inelegant Python solution for LC#665

--- LeetCode/1-1000/601-700/651-675/665. Non-decreasing Array/solution-python.py
+++ LeetCode/1-1000/601-700/651-675/665. Non-decreasing Array/solution-python.py
@@ -2,5 +2,30 @@ from typing import List
 
 
 class Solution:
+    # This is so ugly and inelegant, just lots of if/else blocks checking a
+    # number and its neighbors and to figure out what number to change.
+    # On the other hand, if it's stupid but it works, is it really that
+    # stupid?  Yeah, it still kind of is I think.
+    # It's still in the 90th percentile for speed by LeetCode's metrics, so I
+    # guess everyone else somehow did it even worse than this.
     def checkPossibility(self, nums: List[int]) -> bool:
-        pass
+        if len(nums) < 3:
+            return True
+        increasing = 1
+        for i in range(1, len(nums) - 1):
+            if nums[i - 1] > nums[i]:
+                if nums[i] > nums[i + 1]:
+                    return False
+                elif increasing == 1:
+                    increasing = 0
+                else:
+                    return False
+            elif nums[i] > nums[i + 1]:
+                if increasing == 0:
+                    return False
+                if nums[i - 1] > nums[i + 1]:
+                    nums[i + 1] = nums[i]
+                else:
+                    nums[i] = nums[i + 1]
+                increasing = 0
+        return True
