d77295446295e0978768cb40d7934b661931889b
Alex DuBois <adubois@alumni.uci.edu>
2021-08-23 19:18:46 -0600

    Python solution for LC#674

--- LeetCode/1-1000/601-700/651-675/674. Longest Continuous Increasing Subsequence/solution-python.py
+++ LeetCode/1-1000/601-700/651-675/674. Longest Continuous Increasing Subsequence/solution-python.py
@@ -2,5 +2,20 @@ from typing import List
 
 
 class Solution:
+    # Not much to say about this, it's pretty straight-forward and obvious
+    # what's going on.  As long as nums[i] is less than nums[i + 1], increment
+    # the current counter by 1.  As soon as it's not, check if it's bigger than
+    # whatever the current best is and set best to that if it is.  Also have to
+    # check which is bigger at the end in case the longest subarray runs to the
+    # last index because it won't hit the part that sets best in that case.
     def findLengthOfLCIS(self, nums: List[int]) -> int:
-        pass
+        if len(nums) == 1:
+            return 1
+        best = current = 1
+        for i in range(len(nums) - 1):
+            if nums[i] >= nums[i + 1]:
+                best = max(current, best)
+                current = 1
+            else:
+                current += 1
+        return max(current, best)
