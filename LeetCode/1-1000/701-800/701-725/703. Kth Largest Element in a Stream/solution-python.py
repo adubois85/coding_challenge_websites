d6e53db302ad40288931ae37b3b03a2e403eba47
Alex DuBois <adubois@alumni.uci.edu>
2021-08-26 00:41:42 -0600

    1st solution should now work completely for LC#703

--- LeetCode/1-1000/701-800/701-725/703. Kth Largest Element in a Stream/solution-python.py
+++ LeetCode/1-1000/701-800/701-725/703. Kth Largest Element in a Stream/solution-python.py
@@ -5,23 +5,33 @@ class KthLargest:
     # To initialize the problem, sort the input array, then save the last k
     # elements of that list
     def __init__(self, k: int, nums: List[int]):
-        # self.k = k
-        nums = sorted(nums)
-        self.k_elements = nums[len(nums) - k:]
+        self.k = k
+        nums.sort()
+        # Had to modify the way I select the last k elements; if the initial
+        # input list is shorter than k, it would wrap around and cause issues.
+        # E.g. if the input were [1, -5] and k = 3, it k_elements would only be
+        # [1] as 2 - 3 = -1, so nums[-1:] would only give the last element.
+        end = (len(nums) - k if len(nums) - k > 0 else 0)
+        self.k_elements = nums[end:]
 
     def add(self, val: int) -> int:
+        # If the input list were empty, you'll get out of bounds errors as you
+        # try to compare val with non-existant indices
+        if not self.k_elements:
+            self.k_elements.append(val)
         # If the value we're trying to add is as large or larger than the last
         # value in the sorted array, we can simply add it to the end and remove
         # the first element since val is necessarily bigger than everything
         # else in the list.
-        if val >= self.k_elements[-1]:
-            self.k_elements.pop(0)
+        elif val >= self.k_elements[-1]:
             self.k_elements.append(val)
         # If that isn't true, we can use a binary search to determine where to
         # put the value in our list.  If the value we're trying to add is
         # less than the smallest number in our list, we don't even have to do
         # that and can simply return whatever is already the smallest value.
-        elif val > self.k_elements[0]:
+        # Slight addendum, if we don't yet have k items in our list, we still
+        # need to add the value regardless.
+        elif val > self.k_elements[0] or len(self.k_elements) < self.k:
             start = 0
             end = len(self.k_elements)
             while start != end:
@@ -33,6 +43,8 @@ class KthLargest:
                 else:
                     start = mid + 1
             self.k_elements.insert(end, val)
+        # only need to remove things if we have more than k elements
+        if len(self.k_elements) > self.k:
             self.k_elements.pop(0)
         return self.k_elements[0]
 
