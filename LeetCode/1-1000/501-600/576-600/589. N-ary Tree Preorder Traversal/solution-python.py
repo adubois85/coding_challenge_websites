cbf45ee9ccd9b8d5256edbccd5ce736f7d97e809
Alex DuBois <adubois@alumni.uci.edu>
2021-08-29 18:14:22 -0600

    bad, ugly, slow Python Solution for LC#589

--- LeetCode/1-1000/501-600/576-600/589. N-ary Tree Preorder Traversal/solution-python.py
+++ LeetCode/1-1000/501-600/576-600/589. N-ary Tree Preorder Traversal/solution-python.py
@@ -12,10 +12,36 @@ class Solution:
     # Not quite functional yet, returns list of lists of lists with no simple
     # way to flatten them that's obvious to me.  Putting this on the shelf for
     # bit to work on the next problem
+    # def preorder(self, root: 'Node') -> List[int]:
+    #     order = [root.val]
+    #     if root.children:
+    #         for i, leaf in enumerate(root.children):
+    #             # print(i, leaf)
+    #             order.append(self.preorder(leaf))
+    #     return order
+
+    # This works but is ugly and slow as hell.  Pylama also complains that it's
+    # too complex (too much branching logic)
     def preorder(self, root: 'Node') -> List[int]:
+        try:
+            if root.children is None:
+                return root.val
+        except AttributeError:
+            return root
         order = [root.val]
+        temp = []
         if root.children:
-            for i, leaf in enumerate(root.children):
-                # print(i, leaf)
-                order.append(self.preorder(leaf))
+            try:
+                for leaf in root.children:
+                    temp.append(self.preorder(leaf))
+                    # print(temp)
+            except TypeError:
+                temp.append(self.preorder(root.children))
+            for num in temp:
+                if type(num) is list:
+                    for j in range(len(num)):
+                        order.append(num[j])
+                else:
+                    order.append(num)
+                # temp = self.preorder(leaf.children)
         return order
