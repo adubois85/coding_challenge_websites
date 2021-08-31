2554f1b63f1cbeeaf9f86dde7e77be679d32e6c6
Alex DuBois <adubois@alumni.uci.edu>
2021-08-31 17:51:18 -0600

    simple linear time solution for LC#852

--- LeetCode/1-1000/801-900/851-875/852. Peak Index in a Mountain Array/solution-python.py
+++ LeetCode/1-1000/801-900/851-875/852. Peak Index in a Mountain Array/solution-python.py
@@ -2,5 +2,9 @@ from typing import List
 
 
 class Solution:
+    # Braindead simple solution, but slow-ish.  O(n) time complexity.
     def peakIndexInMountainArray(self, arr: List[int]) -> int:
-        pass
+        index = 1
+        while arr[index] < arr[index + 1]:
+            index += 1
+        return index
