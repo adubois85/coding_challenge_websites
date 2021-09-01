3e59acdc6db67a986e32fd6b4f1aa5891c25ab00
Alex DuBois <adubois@alumni.uci.edu>
2021-08-31 19:22:36 -0600

    kludgey Python solution  for LC#1160

--- LeetCode/1001-2000/1101-1200/1151-1175/1160. Find Words That Can Be Formed by Characters/solution-python.py
+++ LeetCode/1001-2000/1101-1200/1151-1175/1160. Find Words That Can Be Formed by Characters/solution-python.py
@@ -1,6 +1,23 @@
+from collections import Counter
 from typing import List
 
 
 class Solution:
+    # Huh, it works...
+    # Still, not the most efficient method, that's for certain.
     def countCharacters(self, words: List[str], chars: str) -> int:
-        pass
+        count = [Counter(word) for word in words]
+        chars = Counter(chars)
+        total = 0
+        for word in count:
+            temp = 0
+            for k, v in word.items():
+                if v > chars[k]:
+                    temp -= 10000
+                    break
+                else:
+                    temp += v
+            if temp < 0:
+                temp = 0
+            total += temp
+        return total
