91f753bdcb8e3cca9e1e1db18e31f0eef344f9dd
Alex DuBois <adubois@alumni.uci.edu>
2022-07-28 18:18:25 -0600

    python solution for LC#3

--- LeetCode/1-1000/1-100/1-25/3. Longest Substring Without Repeating Characters/solution-python.py
+++ LeetCode/1-1000/1-100/1-25/3. Longest Substring Without Repeating Characters/solution-python.py
@@ -1,3 +1,15 @@
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        # Ah, dynamic programming, we meet again.  I flubbed it initially because
+        # I reset the substring when it encountered a duplicate instead of
+        # starting the new substring from the index after the duplicate.
+        if len(s) == 0:
+            return 0
+        ans = 0
+        substr = []
+        for i in s:
+            if i in substr:
+                ans = max(ans, len(substr))
+                substr = substr[(substr.index(i) + 1):]
+            substr.append(i)
+        return max(ans, len(substr))
