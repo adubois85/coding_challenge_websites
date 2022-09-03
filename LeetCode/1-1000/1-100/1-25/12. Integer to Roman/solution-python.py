dc34c5ae7d958277bcede8c0586b41b41d5d709b
Alex DuBois <adubois@alumni.uci.edu>
2022-09-02 20:03:09 -0600

    python solution for LC#12

--- LeetCode/1-1000/1-100/1-25/12. Integer to Roman/solution-python.py
+++ LeetCode/1-1000/1-100/1-25/12. Integer to Roman/solution-python.py
@@ -1,3 +1,23 @@
 class Solution:
     def intToRoman(self, num: int) -> str:
-        pass
+        numerals = [(1000, 'M'),
+                    (900, 'CM'),
+                    (500, 'D'),
+                    (400, 'CD'),
+                    (100, 'C'),
+                    (90, 'XC'),
+                    (50, 'L'),
+                    (40, 'XL'),
+                    (10, 'X'),
+                    (9, 'IX'),
+                    (5, 'V'),
+                    (4, 'IV'),
+                    (1, 'I')]
+        pos = 0
+        roman = ''
+        while num > 0:
+            while num >= numerals[pos][0]:
+                num -= numerals[pos][0]
+                roman += numerals[pos][1]
+            pos += 1
+        return roman
