9133b350c0eb058c3aef1883e6b010c451239be8
Alex DuBois <adubois@alumni.uci.edu>
2022-10-01 17:51:10 -0600

    formatting cleanup of LC#5, python solution #3

--- LeetCode/1-1000/1-100/1-25/5. Longest Palindromic Substring/solution-python3.py
+++ LeetCode/1-1000/1-100/1-25/5. Longest Palindromic Substring/solution-python3.py
@@ -4,7 +4,7 @@ class Solution:
     # Specifically, it sets the even and odd-length indices, but only checks
     # even ones if the odd one centered at that index fails, probably because
     # the odd-lengthed one will always be longer at the same center point.
-    # This is seems to be a sort of implementation of Manachar's algorithm, 
+    # This is seems to be a sort of implementation of Manachar's algorithm,
     # since the size variable increasing will skip some center points, but it's
     # not a "pure" implementation of it as I've seen described online.
     def longestPalindrome(self, s: str) -> str:
@@ -13,10 +13,9 @@ class Solution:
         start, size = 0, 1
         for i in range(1, len(s)):
             l, r = i - size, i + 1
-            s1, s2 = s[l-1:r], s[l:r] 
+            s1, s2 = s[l-1:r], s[l:r]
             if l - 1 >= 0 and s1 == s1[::-1]:
                 start, size = l - 1, size + 2
             elif s2 == s2[::-1]:
                 start, size = l, size + 1
-                
         return s[start:start+size]
